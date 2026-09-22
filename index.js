13c0230691106a3178f4f3e410f0520f1767a1e3
Mior Sufian <mior.sufian@googlemail.com>
2026-09-22 17:21:49 +0800

    updated to include secret

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config(); 
 const PORT = process.env.PORT || 3000;
 
 app.use(cors()); // Enable CORS for all origins
@@ -9,6 +10,14 @@ app.use(express.json());
 // Routes
 app.get("/", (req, res) => {
   res.send("Welcome to the Express API!");
+});
+
+app.get('/secret', (req, res) => {
+  const providedKey = req.headers['x-api-key'];
+  if (providedKey !== process.env.API_KEY) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  res.json({ message: 'You found the secret data!' });
 });
 
 app.listen(PORT, () => {
